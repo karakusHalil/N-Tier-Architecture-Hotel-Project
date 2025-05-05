ebf5a99b14c3245c10a0c4ff5e12af5f68fd02eb
karakusHalil <karakus_halil@outlook.com>
2025-05-05 18:52:49 +0300

    Implement generic repository pattern with basic CRUD operations

--- ApiConsume/HotelProject.DataAccessLayer/Abstract/IGenericDal.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Abstract/IGenericDal.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HotelProject.DataAccessLayer.Abstract
+﻿namespace HotelProject.DataAccessLayer.Abstract
 {
-    internal interface IGenericDal
+    public interface IGenericDal<T> where T : class
     {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetList();
+        T GetById(int id);
     }
 }
