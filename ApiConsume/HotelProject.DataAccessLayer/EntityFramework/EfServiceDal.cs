de30dbddeaf0d1274a4d37acf9812f1057fa230c
karakusHalil <karakus_halil@outlook.com>
2025-05-05 23:57:24 +0300

    Setup Entity Framework structure and implement repositories for all entities

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfServiceDal.cs
@@ -0,0 +1,14 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EfServiceDal : GenericRepository<Service>, IServicesDal
+    {
+        public EfServiceDal(Context context) : base(context)
+        {
+
+        }
+    }
+}
