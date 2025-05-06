b7b963c3406aeb1cc041aa6c5d3ff9152b94cef5
karakusHalil <karakus_halil@outlook.com>
2025-05-06 15:08:17 +0300

    Add and fixing

---
+++ ApiConsume/HotelProject.WebApi/Controllers/RoomController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult RoomList()
+        {
+            return Ok("Room List");
+        }
+        [HttpPost]
+        public IActionResult AddRoom()
+        {
+            return Ok("Add Room");
+        }
+        [HttpDelete]
+        public IActionResult DeleteRoom()
+        {
+            return Ok("Delete Room");
+        }
+        [HttpPut]
+        public IActionResult UpdateRoom()
+        {
+            return Ok("Update Room");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetRoom()
+        {
+            return Ok("Get Room By Id");
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.WebApi/Startup.cs
@@ -0,0 +1,58 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.BusinessLayer.Concrete;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.EntityFramework;
+using Microsoft.OpenApi.Models;
+
+namespace HotelProject.WebApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<Context>();
+
+            services.AddScoped<IStaffDal, EfStaffDal>();
+            services.AddScoped<IStaffService, StaffManager>();
+
+            services.AddControllers(); // Web API yapısı kullanıyorsan bu gerekli
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelProject.WebApi", Version ="v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                // Swagger'ı sadece geliştirici ortamında açmak mantıklı olur
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "HotelProject.WebApi v1");
+                    c.RoutePrefix = "swagger"; // Swagger açıldığında direkt ana sayfaya düşsün
+                });
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization(); // Bu önemli, varsa [Authorize] için gerekli
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
