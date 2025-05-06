8c21d8ca64eaeb7a4db7133dd6af251a5ae8fae2
karakusHalil <karakus_halil@outlook.com>
2025-05-06 19:29:22 +0300

    Register DAL and Service dependencies for Staff, Services, Testimonial, Room, and Subscribe

--- ApiConsume/HotelProject.WebApi/Startup.cs
+++ ApiConsume/HotelProject.WebApi/Startup.cs
@@ -23,6 +23,18 @@ namespace HotelProject.WebApi
             services.AddScoped<IStaffDal, EfStaffDal>();
             services.AddScoped<IStaffService, StaffManager>();
 
+            services.AddScoped<IServicesDal, EfServiceDal>();
+            services.AddScoped<IServiceService, ServiceManager>();
+
+            services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+            services.AddScoped<ITestimonialService, TestimonialManager>();
+
+            services.AddScoped<IRoomDal, EfRoomDal>();
+            services.AddScoped<IRoomService, RoomManager>();
+
+            services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+            services.AddScoped<ISubscribeService, SubscribeManager>();
+
             services.AddControllers(); // Web API yapısı kullanıyorsan bu gerekli
             services.AddSwaggerGen(c =>
             {
